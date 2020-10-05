1421e023b920921a5fcba201e9ba67874c2ef8bd
Namito Yokota <namitoyokota@Namitos-MacBook-Air.local>
2020-10-05 11:08:35 -0500

    firebase auth init

---
+++ lib/services/auth.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anonymously
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email & password
+
+  // register with email & password
+
+  // sign out
+}
