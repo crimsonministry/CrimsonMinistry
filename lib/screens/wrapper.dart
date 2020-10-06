a544c6ea1f7019cd9f2c9fe867c26d055d2e14fe
Namito Yokota <namitoyokota@Namitos-MacBook-Air.local>
2020-10-05 20:43:25 -0500

    changed name to sign in

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:CrimsonMinistry/screens/home/home.dart';
-import 'package:CrimsonMinistry/screens/auth/auth.dart';
+import 'package:CrimsonMinistry/screens/auth/signin.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either Home or Authenticate Widget
-    return Auth();
+    return SignIn();
   }
 }
