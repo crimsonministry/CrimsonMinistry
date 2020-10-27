9428d047bf5cdb37667c6cd9a969e2e59a4773ca
Namito Yokota <nyokota@crimson.ua.edu>
2020-10-27 12:29:32 -0500

    rsvp from tile

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,14 +1,20 @@
 class Event {
+  final String id;
   final String title;
   final String location;
   final String time;
   final String typeOfEvent;
   final String description;
+  final List rsvp;
+  final String userID;
 
   Event(
-      {this.title,
+      {this.id,
+      this.title,
       this.location,
       this.time,
       this.typeOfEvent,
-      this.description});
+      this.description,
+      this.rsvp,
+      this.userID});
 }
