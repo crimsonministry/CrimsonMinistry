9a6771864ff105d41359925375c8ea509bfc2be6
Namito Yokota <nyokota@crimson.ua.edu>
2020-11-09 10:35:01 -0600

    10 unit test cases

--- test/readEvents_test.dart
+++ test/readEvents_test.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:CrimsonMinistry/services/database.dart';
+import 'package:CrimsonMinistry/models/event.dart';
 
-String userID = '5j9k7uGIhlPh4WJU07famWoSpNR2';
+String userID = 'Rco0ZMq21weyKLtdNtTSiemBV6K2';
 DateTime pickedDate = new DateTime(2020, 11, 15, 8, 15);
 String typeOfEvent = 'Bible Study';
 String title = 'Bible Study at the Quad';
 String location = 'In front of Denny Chimes';
 String description = 'My friend and I are going through Chapter 1 of Matthew';
 
-String newLocation = 'Gorgas Steps';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   final DatabaseService database = DatabaseService();
-  group('CRUD Events :', () {
-    test('Add an event', () {
-      expect(
-          database.addEvent(
-              userID, pickedDate, typeOfEvent, title, location, description),
-          isInstanceOf<Future>());
-    });
+  test('Read events list', () {
+    expect(database.events, isInstanceOf<Stream<List<Event>>>());
   });
 }
--- test/updatePrayer_test.dart
+++ test/updatePrayer_test.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:CrimsonMinistry/services/database.dart';
 
-bool beAnonymous = false;
-String userID = 'Rco0ZMq21weyKLtdNtTSiemBV6K2';
+String documentID = 'KegTfbsJbcwm8WRXxHan';
 String title = 'testTitle';
 String description = 'testDescription';
 
@@ -11,10 +10,8 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   final DatabaseService database = DatabaseService();
-  group('CRUD Prayer Requests :', () {
-    test('Add a prayer request', () {
-      expect(database.addPrayerRequest(userID, beAnonymous, title, description),
-          isInstanceOf<Future>());
-    });
+  test('Update a prayer request', () {
+    expect(database.updatePrayer(documentID, title, description),
+        isInstanceOf<Future>());
   });
 }
