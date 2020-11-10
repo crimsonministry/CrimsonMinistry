a6abfd37bf677c130368a680fd4ba0d1e5e322e4
wabeaumont <wabeaumont@crimson.ua.edu>
2020-11-10 14:40:09 -0600

    Fixed issues with prayers list, restructured it and the prayers model to allow tracking of interaction with prayer posts. Added database functions to assist with this as well.

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -200,11 +200,12 @@ class DatabaseService {
     print('description : $description');
     print('success');
 
+    List<String> prayerInteractions = [];
     return await prayerCollection.document(uid).setData({
       'anonymous': anonymous,
       'title': title,
       'description': description,
-      'count': 0,
+      'prayerInteractions': prayerInteractions,
       'createdAt': Timestamp.now(),
       'userID': userid,
     });
@@ -238,10 +239,17 @@ class DatabaseService {
         anonymous: doc.data['anonymous'] ?? true,
         title: doc.data['title'] ?? '',
         description: doc.data['description'] ?? '',
-        count: doc.data['count'] ?? 0,
+        prayerInteractions: List<String>.from(doc.data['prayerInteractions']) ?? '',
         userID: doc.data['userID'] ?? '',
       );
     }).toList();
+  }
+
+  Future addToPrayerInteractions(String documentID, List<String> prayerInteractions) async {
+    print('added to prayerInteractions');
+    return await prayerCollection.document(documentID).updateData({
+      'prayerInteractions': prayerInteractions,
+    });
   }
 
   Stream<UserData> get userData {
