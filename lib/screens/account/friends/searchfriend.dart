f3b3a6f72189de04192b8da459cefe080c19c329
colemanlcost <clcost@crimson.ua.edu>
2020-11-25 16:57:49 -0600

    more work to friend searching page

--- lib/screens/account/friends/searchfriend.dart
+++ lib/screens/account/friends/searchfriend.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:CrimsonMinistry/services/database.dart';
 import 'package:CrimsonMinistry/models/user.dart';
+import 'package:universal_html/html.dart';
+import 'package:universal_html/prefer_universal/js.dart';
 
 class SearchFriendPage extends StatefulWidget {
   @override
@@ -14,13 +16,14 @@ class SearchFriendPage extends StatefulWidget {
 class _SearchFriendPageState extends State<SearchFriendPage> {
   TextEditingController searchTextEditingController = TextEditingController();
   Future<QuerySnapshot> futureSearchResults;
+  final DatabaseService _data = DatabaseService();
 
   clearTheTextFormField() {
     searchTextEditingController.clear();
   }
 
   searchForUser(String searchUserName) {
-    Future<QuerySnapshot> allUsers = userC
+    Future<QuerySnapshot> allUsers = _data.userCollection
         .where("username", isGreaterThanOrEqualTo: searchUserName)
         .getDocuments();
     setState(() {
@@ -48,24 +51,85 @@ class _SearchFriendPageState extends State<SearchFriendPage> {
             suffixIcon: IconButton(
                 icon: Icon(Icons.clear, color: Colors.white),
                 onPressed: clearTheTextFormField())),
-        onFieldSubmitted: searchForUser(),
+        onFieldSubmitted: searchForUser,
       ),
     );
   }
 }
 
-class _SearchFriendPageState extends State<SearchFriendPage> {
+Container noResultDisplay() {
+  final Orientation orientation = MediaQuery.of(context).orientation;
+  return Container(
+    child: Center(child: ListView(shrinkWrap: true, children: <Widget>[
+      Icon(Icons.group, color: Colors.grey, size: 200.0,),
+      Text("No Users Found :(",
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 60.0,
+      ),
+      ),
+    ],
+    ),
+    ),
+    );
+}
+
+resultFoundDisplay() {
+  return FutureBuilder(
+    future: futureSearchResults,
+    builder: (context, dataSnapshot) {
+      if(!dataSnapshot.hasData) {
+        return circularProgress();
+      }
+
+      List<UserResults> searchUsersResult = [];
+      dataSnapshot.data.documents.forEach((document){
+        User eachUser = User.fromDocument(document);
+        UserResults userResults = UserResults(eachUser);
+        searchUsersResult.add(userResults);
+      });
+
+      return ListView(children: searchUsersResult);
+
+    },
+    );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: searchFriendPageHeader(),
+      body: futureSearchResults == null ? noResultDisplay() : resultFoundDisplay(),
+
     );
   }
 }
 
 class UserResults extends StatelessWidget {
+  final User eachUser;
+  UserResults(this.eachUser);
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Text("User Results");
+    return Padding(padding: EdgeInsets.all(4.0),
+    child: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => print("tapped"),
+            child: ListTile(
+              title: Text(
+                eachUser.uid,
+                style: TextStyle(color: Colors.black, fontSize: 16.0),
+              ),
+              subtitle: Text("@username",
+              style: TextStyle(color: Colors.black, fontSize: 13.0,),
+            ),
+          )
+        ],)
+    ),
+    );
   }
 }
