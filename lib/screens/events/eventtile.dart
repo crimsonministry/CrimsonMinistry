4b11fab8b5cdafc3e232effc86aa8448db444a5c
Namito Yokota <nyokota@crimson.ua.edu>
2020-11-26 11:23:56 -0600

    new icons in events tile

--- lib/screens/events/eventtile.dart
+++ lib/screens/events/eventtile.dart
@@ -6,18 +6,44 @@ class EventTile extends StatelessWidget {
   final Event event;
   EventTile({this.event});
 
+  switchIcons(String eventType) {
+    switch (eventType) {
+      case 'Bible Study':
+        return Icon(
+          Icons.book,
+          color: Colors.lightGreen[700],
+          size: 50,
+        );
+        break;
+      case 'Mission':
+        return Icon(
+          Icons.explore,
+          color: Colors.yellow[700],
+          size: 50,
+        );
+        break;
+      case 'Volunteer':
+        return Icon(
+          Icons.favorite,
+          color: Colors.deepOrange[700],
+          size: 50,
+        );
+        break;
+      case 'Worship':
+        return Icon(
+          Icons.radio,
+          color: Colors.cyan[700],
+          size: 50,
+        );
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-          leading: (event.typeOfEvent == 'Worship')
-              ? Icon(
-                  Icons.audiotrack,
-                  color: Colors.blueGrey[900],
-                  size: 40,
-                )
-              : Icon(Icons.import_contacts,
-                  color: Colors.blueGrey[900], size: 40),
+          leading: switchIcons(event.typeOfEvent),
           title: Text(event.title),
           subtitle: Text(
               '${event.dateTime.month}/${event.dateTime.day}/${event.dateTime.year} @ ${event.dateTime.hour}:${event.dateTime.minute}'),
